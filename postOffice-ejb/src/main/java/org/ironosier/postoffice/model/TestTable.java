dd3313a229a70454fc68c4ce0f57bedd037f2225
Vladislav Ivanov <ironosier@gmail.com>
2018-01-25 21:59:03 +0300

    add DS and depended libraries

---
+++ postOffice-ejb/src/main/java/org/ironosier/postoffice/model/TestTable.java
@@ -0,0 +1,15 @@
+package org.ironosier.postoffice.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "test_table")
+public class TestTable implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+
+}
