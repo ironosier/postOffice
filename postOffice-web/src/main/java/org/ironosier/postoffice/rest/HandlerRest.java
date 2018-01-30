f6e6ceb9a59b29d71e05ba88b764ffa309902244
Vladislav Ivanov <ironosier@gmail.com>
2018-01-30 23:42:09 +0300

    refuse rest

--- postOffice-web/src/main/java/org/ironosier/postoffice/rest/HandlerRest.java
+++
@@ -1,9 +0,0 @@
-package org.ironosier.postoffice.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class HandlerRest extends Application {
-
-}
--- postOffice-web/src/main/java/org/ironosier/postoffice/vaadin/EntryVaadin.java
+++ postOffice-web/src/main/java/org/ironosier/postoffice/vaadin/EntryVaadin.java
@@ -1,21 +1,17 @@
 package org.ironosier.postoffice.vaadin;
 
-import java.util.List;
 
-import javax.enterprise.context.ApplicationScoped;
+
 import javax.inject.Inject;
-import javax.ws.rs.core.Response;
 
 import org.ironosier.postoffice.model.TestTable;
-import org.ironosier.postoffice.rest.AuthRest;
-import org.ironosier.postoffice.rest.TestTableRest;
 import org.ironosier.postoffice.service.TestTableService;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.Label;
@@ -25,18 +21,11 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+@SuppressWarnings("serial")
+@Theme("valo")
 @CDIUI("")
 public class EntryVaadin extends UI {
 
-//	@Inject
-//	private TestTableRest testTableRest;
-//
-//	@Inject
-//	private AuthRest auth;
-	
-//	private Label token;
-//	private Label securedInfo;
-	
 	@Inject
 	private TestTableService testTableService;
 
@@ -60,22 +49,13 @@ public class EntryVaadin extends UI {
 		});
 
 		Label label = new Label("Ведите логин и пароль");
-		
-//		Button auth = new Button("Вход", e -> {
-//			login(login.getValue(), pass.getValue());
-//		});
-		
-//		token = new Label("token");
-//		securedInfo = new Label("первая запись");
+	
 		
 		FormLayout content = new FormLayout();
-//		content.addComponent(securedInfo);
-//		content.addComponent(token);
 		content.addComponent(label);
 		content.addComponent(login);
 		content.addComponent(pass);
 		content.addComponent(grid);
-//		content.addComponent(auth);
 		
 
 		Panel panel = new Panel("Авторизация");
@@ -91,21 +71,4 @@ public class EntryVaadin extends UI {
 		setContent(layout);
 
 	}
-
-//	private void login(String name, String pass) {
-//		TestTable testTable = new TestTable();
-//		testTable.setName(name);
-//		testTable.setPass(Integer.parseInt(pass));
-//		
-//		Response resp = auth.login(testTable);
-//		
-//		if (resp.getStatus() ==200) {
-//			token.setValue(resp.getEntity().toString());
-//		}else {
-//			token.setValue(resp.getStatusInfo().toString());
-//		}
-//		TestTable test = (TestTable)testTableRest.getFirst().getEntity();
-//		securedInfo.setValue(test.getName() + " " + test.getPass());
-//		
-//	}
 }
